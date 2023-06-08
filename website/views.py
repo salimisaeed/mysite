1fb906c8d8230ade121519e0aa7b59234e53a4b0
saeed salimi shad <saeedsalimi.ir@gmail.com>
2023-06-08 21:18:02 +0330

    created render html files
    created template & 3 html file for test

--- website/views.py
+++ website/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def index_view(request):
-    return HttpResponse("<h1>HOME PAGE</h1>")
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return JsonResponse({"Name" : "saeed", 
-                         "LastName" : "salimi shad"})
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return JsonResponse({"Phone": "09391701380"})
+    return render(request,'website/contact.html')
